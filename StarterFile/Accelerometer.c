fd4bd896989d6cd5d735040fbb3f56348a43b644
andersh4 <anders.hunt367@gmail.com>
2021-03-11 15:11:44 -0800

    accelerometer added

---
+++ StarterFile/Accelerometer.c
@@ -0,0 +1,10 @@
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include "Accelerometer.h"
+#include <Arduino.h>
+#define xOffset 0;
+#define yOffset 0;
+#define zOffset 0;
+
+    void updateDisplacement(float* xDisplacement, float* yDisplacement, float* zDisplacement, )
---
+++ StarterFile/Accelerometer.h
@@ -0,0 +1,39 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef Accelerometer_H_
+#define Accelerometer_H_
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+
+typedef struct AccelerometerTaskData{
+
+    float* xAcc;
+    float* yAcc;
+    float* zAcc;
+
+    float* xVel;
+    float* yVel;
+    float* zVel;
+
+    float* xDistance;
+    float* xDisplacement;
+    float* yDistance;
+    float* yDisplacement;
+    float* zDistance;
+    float* zDisplacement;
+
+
+}AccelerometerData;
+
+void AccelerometerTask (void*);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
