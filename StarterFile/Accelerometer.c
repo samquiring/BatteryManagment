45799f7896db700f77d17fb5997fde75b619809f
andersh4 <anders.hunt367@gmail.com>
2021-03-11 19:32:58 -0800

    Update Accelerometer.c

--- StarterFile/Accelerometer.c
+++ StarterFile/Accelerometer.c
@@ -26,9 +26,8 @@
 
     void updateDisplacement(float* xDisplacement, float* xAcc, float* xVel, float* yDisplacement, float* yAcc, float* yVel, float* zDisplacement, float* zAcc, float* zVel, float* timeBase) {
 
-        totalG = sqrt((*xAcc * *xAcc) + (*yAcc * *yAcc) + (*zAcc * *zAcc))
 
-        if ((*zAcc > 980.0 - movementErrorMargin) && (*zAcc < 980.0 - movementErrorMargin) {
+        if ((*zAcc > 980.0 - movementErrorMargin) && (*zAcc < 980.0 - movementErrorMargin)) {
             *xDisplacement = *xDisplacement + (*xVel * *timeBase) + 0.5 * *xAcc * (*timeBase * *timeBase);
             *yDisplacement = *yDisplacement + (*yVel * *timeBase) + 0.5 * *yAcc * (*timeBase * *timeBase);
             *zDisplacement = *zDisplacement + (*zVel * *timeBase) + 0.5 * (*zAcc - 980.0) * (*timeBase * *timeBase);
@@ -37,7 +36,7 @@
 
     void updateDistance(float* totalDistance, float* xAcc, float* xVel, float* yAcc, float* yVel, float* zAcc, float* zVel, float* timeBase){
 
-        if ((*zAcc > 980.0 - movementErrorMargin) && (*zAcc < 980.0 - movementErrorMargin) {
+        if ((*zAcc > 980.0 - movementErrorMargin) && (*zAcc < 980.0 - movementErrorMargin)) {
             float xChange = (*xVel * *timeBase) + 0.5 * *xAcc * (*timeBase * *timeBase);
             float yChange = (*yVel * *timeBase) + 0.5 * *yAcc * (*timeBase * *timeBase);
             float zChange = (*zVel * *timeBase) + 0.5 * (*zAcc - 980.0) * (*timeBase * *timeBase);
